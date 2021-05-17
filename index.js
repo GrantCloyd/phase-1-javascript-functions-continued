afb10496df2a817db831286d566a2969485c2f63
Grant Cloyd <dgcloyd@gmail.com>
2021-05-17 15:43:31 -0500

    Another one down

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+
+const saturdayFun = (activity='roller-skate') => `This Saturday, I want to ${activity}!`;
+
+const mondayWork = (task='go to the office') => `This Monday, I will ${task}.`
+
+const wrapAdjective = (exclamation='*') => {
+    const innerFunction = (adj='special') => `You are ${exclamation}${adj}${exclamation}!`
+    return innerFunction;
+}
